a5acd81415f5a59e8a65088b36d638069ebdf690
sleepedwind <sleepedwind@163.com>
2025-03-09 23:08:18 +0800

    vue工程案例

---
+++ vue_project/vue-project1/src/views/ApiDemo.vue
@@ -0,0 +1,24 @@
+<script setup>
+    //引入 ref 函数
+    import { ref,onMounted } from "vue";
+
+    //声明响应式数据
+    const count = ref(0);
+    function increment() {
+        count.value++;
+    };
+
+    //钩子函数
+    onMounted(() => {
+        console.log('Vue mounted')
+    });
+
+</script>
+
+<template>
+    <button @click="increment">count:{{ count  }}</button>
+</template>
+
+<style scoped>
+
+</style>
